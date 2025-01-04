f8ed1c731966e31481c352aa61194a9d51ce9e2a
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-04 12:52:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 const makeChange = (c) => {
   // your name here
+
+	const change={
+		q:0,
+		d:0,
+		n:0,
+		p:0
+	};
+	
+	const coinValues={
+		d:25,
+		d:10,
+		n:5,
+		p:1
+	};
+
+	change.q=Math.floor(c/coinValues.q);
+	c%=coinValues.q;
+
+	change.d=Math.floor(c/coinValues.d);
+	c%=coinValues.d;
+
+	change.n=Math.floor(c/coinValues.n);
+	c%=coinValues.n;
+
+	change.p=c;
+
+	return change;
 };
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
