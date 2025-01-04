2fb6c17ceca4afdb40661224d43776b3e2e524f8
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-04 12:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-const makeChange = (c) => {
-  // your name here
-
-	const change={
-		q:0,
-		d:0,
-		n:0,
-		p:0
-	};
-	
-	const coinValues={
-		d:25,
-		d:10,
-		n:5,
-		p:1
-	};
-
-	change.q=Math.floor(c/coinValues.q);
-	c%=coinValues.q;
-
-	change.d=Math.floor(c/coinValues.d);
-	c%=coinValues.d;
-
-	change.n=Math.floor(c/coinValues.n);
-	c%=coinValues.n;
-
-	change.p=c;
-
-	return change;
+const makeChange=(c)=>{
+    const change={
+        q:0,
+        d:0,
+        n:0,
+        p:0
+    }
+    
+    const coinValues={
+        q:25,
+        d:10,
+        n:5,
+        p:1
+    }
+    
+    change.q=Math.floor(c/coinValues.q);
+    c %= coinValues.q;
+    
+    change.d=Math.floor(c/coinValues.d);
+    c %= coinValues.d;
+    
+    change.n=Math.floor(c/coinValues.n);
+    c %= coinValues.n;
+    
+    change.p=c;
+    
+    return change;
+    
 };
 
 
